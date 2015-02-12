45af7bb2586bcf82bcbf1227700125a0d1370d8f
skoppula <skoppula@mit.edu>
2015-02-11 23:16:05 -0500

    second commit

---
+++ pset1/onepad.py
@@ -0,0 +1,32 @@
+CT1 = 'd2 6b a5 0d 27 6a 34 2d 8e 53 0e'.split(' ')
+CT2 = 'de 6e a7 00 30 74 34 2b 8e 51 11'.split(' ')
+
+CT1_num = [int(byte, 16) for byte in CT1]
+CT2_num = [int(byte, 16) for byte in CT2]
+
+alphabet = [i+65 for i in range(58)]
+
+PT1 = []
+PT2 = []
+
+for i, val in enumerate(CT1_num):
+    #pad_byte_possibilities = [a^val for a in alphabet if (a^val>64 and a^val<123)]
+    pad_byte_possibilities = []
+    for a in alphabet:
+        if(a^val>64 and a^val<123):
+            pad_byte_possibilities.append(a^val)
+    
+    lst1 = []
+    lst2 = []
+    for possibility in pad_byte_possibilities:
+        temp = possibility ^ CT2_num[i]
+        if temp > 64 and temp < 123:
+            lst2.append(str(unichr(temp)))
+            lst1.append(str(unichr(possibility ^ CT1_num[i])))
+
+    PT2.append(lst2)
+    PT1.append(lst1)
+
+print PT1
+print PT2
+
